4d8ac45e180ff05d75c6c00d7a9c4a8de9cb3c58
Lucas Martins <dev.lucasmartins@gmail.com>
2019-09-09 16:45:49 -0300

    adjustments to user request

--- src/main/java/br/com/ottimizza/application/repositories/users/UsersRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/application/repositories/users/UsersRepositoryImpl.java
@@ -7,7 +7,6 @@ import br.com.ottimizza.application.model.user.QUser;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import com.querydsl.core.QueryFactory;
 import com.querydsl.jpa.impl.JPAQuery;
 
 import org.springframework.data.domain.Page;
@@ -23,39 +22,62 @@ public class UsersRepositoryImpl implements UsersRepositoryCustom {
 
     private QUser user = QUser.user;
 
+    // @Override
+    // public Page<User> fetchAll(UserDTO filter, Pageable pageable) {
+
+    // Integer pageIndex = pageable.getPageNumber();
+    // Integer pageSize = pageable.getPageSize();
+
+    // long totalElements = 0;
+
+    // JPAQuery<User> query = new JPAQuery<User>(em).from(user);
+
+    // // Email
+    // if (filter.getEmail() != null && !filter.getEmail().isEmpty()) {
+    // query.where(user.email.like("%" + filter.getEmail() + "%"));
+    // }
+
+    // // Type
+    // if (filter.getType() != null && filter.getType() > 0) {
+    // query.where(user.type.eq(filter.getType()));
+    // }
+
+    // // totalPages = (totalElements + pageSize - 1) / pageSize;
+
+    // totalElements = query.fetchCount();
+
+    // // hasNext = (pageIndex + 1) * pageSize < totalElements;
+
+    // if (pageSize != null && pageSize > 0) {
+    // query.limit(pageSize);
+    // if (pageIndex != null && pageIndex > 0) {
+    // query.offset(pageSize * pageIndex);
+    // }
+    // }
+
+    // return new PageImpl<User>(query.fetch(), pageable, totalElements);
+    // }
+
     @Override
     public Page<User> fetchAll(UserDTO filter, Pageable pageable) {
-
-        Integer pageIndex = pageable.getPageNumber();
-        Integer pageSize = pageable.getPageSize();
-
         long totalElements = 0;
-
         JPAQuery<User> query = new JPAQuery<User>(em).from(user);
-
-        // Email
+        if (filter.getId() != null) {
+            query.where(user.id.eq(filter.getId()));
+        }
         if (filter.getEmail() != null && !filter.getEmail().isEmpty()) {
             query.where(user.email.like("%" + filter.getEmail() + "%"));
         }
-
-        // Type
+        if (filter.getUsername() != null && !filter.getUsername().isEmpty()) {
+            query.where(user.email.like(filter.getUsername()));
+        }
         if (filter.getType() != null && filter.getType() > 0) {
             query.where(user.type.eq(filter.getType()));
         }
 
-        // totalPages = (totalElements + pageSize - 1) / pageSize;
-
         totalElements = query.fetchCount();
-
-        // hasNext = (pageIndex + 1) * pageSize < totalElements;
-
-        if (pageSize != null && pageSize > 0) {
-            query.limit(pageSize);
-            if (pageIndex != null && pageIndex > 0) {
-                query.offset(pageSize * pageIndex);
-            }
-        }
-
+        query.limit(pageable.getPageSize());
+        query.offset(pageable.getPageSize() * pageable.getPageNumber());
         return new PageImpl<User>(query.fetch(), pageable, totalElements);
     }
 
@@ -63,11 +85,15 @@ public class UsersRepositoryImpl implements UsersRepositoryCustom {
     public Page<User> fetchAll(UserDTO filter, Pageable pageable, User authorizedUser) {
         long totalElements = 0;
         JPAQuery<User> query = new JPAQuery<User>(em).from(user);
-        // Email
+        if (filter.getId() != null) {
+            query.where(user.id.eq(filter.getId()));
+        }
         if (filter.getEmail() != null && !filter.getEmail().isEmpty()) {
             query.where(user.email.like("%" + filter.getEmail() + "%"));
         }
-        // Type
+        if (filter.getUsername() != null && !filter.getUsername().isEmpty()) {
+            query.where(user.email.like(filter.getUsername()));
+        }
         if (filter.getType() != null && filter.getType() > 0) {
             query.where(user.type.eq(filter.getType()));
         }
