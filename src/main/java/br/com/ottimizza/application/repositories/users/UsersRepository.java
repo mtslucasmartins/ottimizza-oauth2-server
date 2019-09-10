1676904658b3a032febb50f00e0c65fe2502ca18
Lucas Martins <dev.lucasmartins@gmail.com>
2019-09-10 12:50:10 -0300

    added organizationdto to user response

--- src/main/java/br/com/ottimizza/application/repositories/users/UsersRepository.java
+++ src/main/java/br/com/ottimizza/application/repositories/users/UsersRepository.java
@@ -33,7 +33,7 @@ public interface UsersRepository extends PagingAndSortingRepository<User, BigInt
             + " 	)                                                                      \n"
             + " )                                                                          \n"
             + " AND (u.type = 2)                                                           \n"
-            + " AND (u.username LIKE :username OR :username is null)                          \n"
+            + " AND (:username is null OR u.username = :username)                          \n"
             + " AND (u.email LIKE CONCAT('%',:email,'%') OR :email is null)               \n"
             + " AND (u.first_name LIKE CONCAT('%',:firstName,'%') OR :firstName is null)  \n"
             + " AND (u.last_name LIKE CONCAT('%',:lastName,'%') OR :lastName is null)     \n", nativeQuery = true)
