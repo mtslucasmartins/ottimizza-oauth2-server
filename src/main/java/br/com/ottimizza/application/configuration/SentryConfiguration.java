b8d25d7d26212c96811ea32c42833438694a6031
Lucas Martins <dev.lucasmartins@gmail.com>
2019-10-16 21:05:51 -0300

    added sentry configuration

---
+++ src/main/java/br/com/ottimizza/application/configuration/SentryConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.ottimizza.application.configuration;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+public class SentryConfiguration {
+
+    @Bean
+    public HandlerExceptionResolver sentryExceptionResolver() {
+        return new io.sentry.spring.SentryExceptionResolver();
+    }
+
+    @Bean
+    public ServletContextInitializer sentryServletContextInitializer() {
+        return new io.sentry.spring.SentryServletContextInitializer();
+    }
+
+}
