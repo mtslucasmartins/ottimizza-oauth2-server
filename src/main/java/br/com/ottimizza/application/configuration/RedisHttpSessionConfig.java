53614dcb343a1c34e87b11485504b2fd274b759a
Lucas Martins <dev.lucasmartins@gmail.com>
2019-10-28 12:00:10 -0300

    added spring session and redis configuration

---
+++ src/main/java/br/com/ottimizza/application/configuration/RedisHttpSessionConfig.java
@@ -0,0 +1,8 @@
+package br.com.ottimizza.application.configuration;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class RedisHttpSessionConfig {
+
+}
