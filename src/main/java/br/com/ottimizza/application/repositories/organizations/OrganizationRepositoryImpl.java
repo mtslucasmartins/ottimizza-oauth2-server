9713b0f304f8e320c25aed54608df4a9978975e6
Lucas Martins <dev.lucasmartins@gmail.com>
2019-12-26 16:16:06 -0300

    added paginate

--- src/main/java/br/com/ottimizza/application/repositories/organizations/OrganizationRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/application/repositories/organizations/OrganizationRepositoryImpl.java
@@ -54,7 +54,7 @@ public class OrganizationRepositoryImpl implements OrganizationRepositoryCustom 
             .innerJoin(userOrganization)
                 .on(userOrganization.organization.id.eq(organization.id)
                 .and(userOrganization.user.id.eq(id)));
-        // totalElements = filter(query, filter);  
+        totalElements = filter(query, filter);  
         sort(query, pageable, Organization.class, QORGANIZATION_NAME);
         paginate(query, pageable);
         return new PageImpl<Organization>(query.fetch(), pageable, totalElements);
@@ -84,27 +84,34 @@ public class OrganizationRepositoryImpl implements OrganizationRepositoryCustom 
     }
 
     private <T> long filter(JPAQuery<T> query, OrganizationDTO filter) {
-        if (filter.getId() != null) 
+        if (filter.getId() != null) {
+            System.out.println("1");
             query.where(organization.id.eq(filter.getId()));
-        
-        if (filter.getExternalId() != null && !filter.getExternalId().isEmpty()) 
+        }
+        if (filter.getExternalId() != null && !filter.getExternalId().isEmpty()) {
+            System.out.println("2");
             query.where(organization.externalId.like(filter.getExternalId()));
-        
-        if (filter.getName() != null && !filter.getName().isEmpty()) 
+        }
+        if (filter.getName() != null && !filter.getName().isEmpty()) {
+            System.out.println("3");
             query.where(organization.name.like("%" + filter.getName() + "%"));
-        
-        if (filter.getCnpj() != null && !filter.getCnpj().isEmpty()) 
+        }
+        if (filter.getCnpj() != null && !filter.getCnpj().isEmpty()) {
+            System.out.println("4");
             query.where(organization.cnpj.like(filter.getCnpj()));
-        
-        if (filter.getCodigoERP() != null && !filter.getCodigoERP().isEmpty()) 
+        }
+        if (filter.getCodigoERP() != null && !filter.getCodigoERP().isEmpty()) {
+            System.out.println("5");
             query.where(organization.codigoERP.like(filter.getCodigoERP() + "%"));
-        
-        if (filter.getType() != null) 
+        }
+        if (filter.getType() != null) {
+            System.out.println("6");
             query.where(organization.type.eq(filter.getType()));
-        
-        if (filter.getOrganizationId() != null) 
+        }
+        if (filter.getOrganizationId() != null) {
+            System.out.println("7");
             query.where(organization.organization.id.eq(filter.getOrganizationId()));
-        
+        }
         return query.fetchCount();
     }
 
