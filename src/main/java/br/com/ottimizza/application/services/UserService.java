bd94485d3403cf58c0609591ece9f20aef1b45ec
Lucas Martins <dev.lucasmartins@gmail.com>
2019-12-26 15:52:23 -0300

    teste 1

--- src/main/java/br/com/ottimizza/application/services/UserService.java
+++ src/main/java/br/com/ottimizza/application/services/UserService.java
@@ -185,14 +185,15 @@ public class UserService {
 
     public Page<OrganizationDTO> fetchOrganizations(BigInteger id, OrganizationDTO filter,
             SearchCriteria searchCriteria, Principal principal) throws OrganizationNotFoundException, Exception {
-        User authorizedUser = findByUsername(principal.getName());
+        // User authorizedUser = findByUsername(principal.getName());
 
         System.out.println(">>" + id);
-
+        User authorizedUser = new User();
+        authorizedUser.setId(id);
         // Garante que não terá acesso a dados de outras contabilidades.
         // filter.setOrganizationId(authorizedUser.getOrganization().getId());
 
-        return organizationRepository.fetchAllByCustomerId(id, filter, OrganizationDTO.getPageRequest(searchCriteria))
+        return organizationRepository.fetchAllByCustomerId(filter, OrganizationDTO.getPageRequest(searchCriteria),authorizedUser)
                 .map(OrganizationDTO::fromEntity);
     }
 
