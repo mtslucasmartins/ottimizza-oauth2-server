0330b455581903adae40012bcbeb50f10997e8fd
Lucas Martins <dev.lucasmartins@gmail.com>
2020-03-24 22:00:22 -0300

    adjustmnts

--- src/main/java/br/com/ottimizza/application/configuration/session/HttpSessionConfig.java
+++
@@ -1,8 +0,0 @@
-package br.com.ottimizza.application.configuration.session;
-
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@EnableRedisHttpSession 
-public class HttpSessionConfig {
-
-}
