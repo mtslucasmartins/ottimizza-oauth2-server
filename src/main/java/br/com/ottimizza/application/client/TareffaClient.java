f8ede1731be57380d2dc18261bcf8115dbc63684
CrispimPDs <crispimpds5@gmail.com>
2020-04-08 10:17:41 -0300

    Ajuste de parametro url - Tareffa

--- src/main/java/br/com/ottimizza/application/client/TareffaClient.java
+++ src/main/java/br/com/ottimizza/application/client/TareffaClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "${tareffa.service.name}", url = "tareffa.service.url")
+@FeignClient(name = "${tareffa.service.name}", url = "${tareffa.service.url}")
 public interface TareffaClient {
     
     @PostMapping("/services/usuarios/password")
